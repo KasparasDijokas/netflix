dfc95cb01ae4e0d4f8c6f8eec3acea9163ee2083
Kasparas Dijokas <44749080+Kasparas17@users.noreply.github.com>
2020-11-29 17:04:54 +0200

    bug fix

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import 'normalize.css';
-import { GlobalStyles } from "./globalStyles";
+// import { GlobalStyles } from "./globalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
+    {/* <GlobalStyles /> */}
     <App />
   </React.StrictMode>,
   document.getElementById("root")
